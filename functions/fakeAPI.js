82401da28acd3b09e1eb8e1af56ed59ee1f31055
Abdulaki <abdulakimustefa@gmail.com>
2024-08-29 20:54:35 +0300

    all origin

--- functions/fakeAPI.js
+++ functions/fakeAPI.js
@@ -1,13 +1,18 @@
 // functions/fakeAPI.js
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 exports.handler = async (event, context) => {
-  const filePath = path.join(__dirname, 'question.json');
-  const data = fs.readFileSync(filePath, 'utf8');
-  
+  const filePath = path.join(__dirname, "question.json");
+  const data = fs.readFileSync(filePath, "utf8");
+
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Allow requests from all origins
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
     body: data,
   };
 };
